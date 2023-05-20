8e08da425c4855994cbd56a12e507964cae490bd
Mario Garcia <mgarciac444@gmail.com>
2023-05-21 01:15:19 +0200

    añadir archivos FXML

--- src/main/java/com/example/testdepersonalidad/FinalController.java
+++ src/main/java/com/example/testdepersonalidad/FinalController.java
@@ -1,4 +1,36 @@
 package com.example.testdepersonalidad;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FinalController {
+
+    @FXML
+    private Button botonReiniciar;
+
+    @FXML
+    private ImageView imagen;
+
+    @FXML
+    private Text nombrePersonaje;
+
+    @FXML
+    void Reiniciar(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
 }
